97e7b5ed689c194c60882b128791f3e5eb485fe2
luciakim22 <luciakim22@naver.com>
2025-08-22 09:05:05 +0900

    feat(utilValidator): 리팩토링 이전 리뷰 필요

--- src/validators/utilValidator.ts
+++ src/validators/utilValidator.ts
@@ -1,20 +1,5 @@
 import { create, object, optional, refine, size, string } from 'superstruct'
 
-// const UTIL_VALIDATION_ERRORS = {
-//   // tooShort: ''
-//   name: '이름 형식에 맞지 않습니다.',
-//   email: '이메일 형식에 맞지 않습니다.',
-//   password: '비밀번호는 영문, 숫자 조합 8~16자리로 입력해주세요',
-//   phoneNumber: '전화번호 형식에 맞지 않습니다(하이픈 포함 필요)'
-// }
-
-// function trimmed(value: any) {
-//   if (value.trim().length >= 1) {
-//     return value.trim()
-//   }
-//   throw new Error ''
-// }
-
 const utilValidator = {
   name: refine(size(string(), 2, 50), 'nameError', (value) => {
     return /^[가-힣]+$/.test(value) || /^[a-zA-Z\s]+$/.test(value);
