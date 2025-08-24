83506c081a59cc5e6185946dae555c49f0a24a0e
luciakim22 <luciakim22@naver.com>
2025-08-25 02:30:25 +0900

    feat:merging pulls

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import express, { Express } from 'express';
-import multer from 'multer';
+import { errorHandler } from './middlewares/errorHandler';
 
 dotenv.config();
 
@@ -14,6 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/images');
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Starting...'));
