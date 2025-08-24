c29e597918d5ffa745a5fd99ebe36ebb4f807a2b
nodeJun <ksh03196@gmail.com>
2025-08-25 08:59:35 +0900

    fix(contract): 에러핸들러 PR과 충돌 해결

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use('/api/contracts', contractRouter);
 app.use(errorHandler);
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Starting...'));
