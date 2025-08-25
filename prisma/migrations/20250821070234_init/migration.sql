ed04497ea7584069838326f083a84a5db3a07860
luciakim22 <luciakim22@naver.com>
2025-08-25 14:37:39 +0900

    fix(feature/imageUpload): migrations 복구 및 public 폴더 삭제&.gitignore 추가2

--- prisma/migrations/20250821070234_init/migration.sql
+++ prisma/migrations/20250821070234_init/migration.sql
@@ -1,17 +1,109 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "public"."Gender" AS ENUM ('male', 'female');
 
-  - You are about to drop the `Meeting` table. If the table is not empty, all the data it contains will be lost.
+-- CreateEnum
+CREATE TYPE "public"."AgeGroup" AS ENUM ('10대', '20대', '30대', '40대', '50대', '60대', '70대', '80대');
 
-*/
--- DropForeignKey
-ALTER TABLE "public"."Meeting" DROP CONSTRAINT "Meeting_contractId_fkey";
+-- CreateEnum
+CREATE TYPE "public"."Region" AS ENUM ('서울', '경기', '인천', '강원', '충북', '충남', '세종', '대전', '전북', '전남', '광주', '경북', '경남', '대구', '울산', '부산', '제주');
 
--- DropTable
-DROP TABLE "public"."Meeting";
+-- CreateEnum
+CREATE TYPE "public"."CarStatus" AS ENUM ('possession', 'contractProceeding', 'contractCompleted');
+
+-- CreateEnum
+CREATE TYPE "public"."ContractStatus" AS ENUM ('carInspection', 'priceNegotiation', 'contractDraft', 'contractSuccessful', 'contractFailed');
 
 -- CreateTable
-CREATE TABLE "public"."Meetings" (
+CREATE TABLE "public"."Company" (
+    "id" SERIAL NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "companyCode" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "employeeNumber" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "companyId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Customer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" "public"."Gender" NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "ageGroup" "public"."AgeGroup",
+    "region" "public"."Region",
+    "email" TEXT,
+    "memo" TEXT,
+    "companyId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Car" (
+    "id" SERIAL NOT NULL,
+    "carNumber" TEXT NOT NULL,
+    "manufacturingYear" INTEGER NOT NULL,
+    "mileage" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "accidentCount" INTEGER NOT NULL,
+    "explanation" TEXT,
+    "accidentDetails" TEXT,
+    "status" "public"."CarStatus" NOT NULL,
+    "model" TEXT NOT NULL,
+    "companyId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."CarModel" (
+    "model" TEXT NOT NULL,
+    "manufacturer" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "CarModel_pkey" PRIMARY KEY ("model")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Contract" (
+    "id" SERIAL NOT NULL,
+    "status" "public"."ContractStatus" NOT NULL,
+    "resulutionDate" TIMESTAMP(3) NOT NULL,
+    "contractPrice" INTEGER NOT NULL,
+    "carId" INTEGER NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "companyId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contract_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Meeting" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "alarms" TEXT[],
@@ -19,8 +111,61 @@ CREATE TABLE "public"."Meetings" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Meetings_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Meeting_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."ContractDocument" (
+    "id" SERIAL NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "contractId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ContractDocument_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_companyCode_key" ON "public"."Company"("companyCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_employeeNumber_key" ON "public"."User"("employeeNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_email_key" ON "public"."Customer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Car_carNumber_key" ON "public"."Car"("carNumber");
+
 -- AddForeignKey
-ALTER TABLE "public"."Meetings" ADD CONSTRAINT "Meetings_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "public"."Contract"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "public"."Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Customer" ADD CONSTRAINT "Customer_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "public"."Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Car" ADD CONSTRAINT "Car_model_fkey" FOREIGN KEY ("model") REFERENCES "public"."CarModel"("model") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Car" ADD CONSTRAINT "Car_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "public"."Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Contract" ADD CONSTRAINT "Contract_carId_fkey" FOREIGN KEY ("carId") REFERENCES "public"."Car"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Contract" ADD CONSTRAINT "Contract_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "public"."Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Contract" ADD CONSTRAINT "Contract_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Contract" ADD CONSTRAINT "Contract_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "public"."Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Meeting" ADD CONSTRAINT "Meeting_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "public"."Contract"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ContractDocument" ADD CONSTRAINT "ContractDocument_contractId_fkey" FOREIGN KEY ("contractId") REFERENCES "public"."Contract"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
