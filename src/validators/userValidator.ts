d794ca58e73e9fafb15cd3808ec967d2d8dc0dcf
Seracine <csyo4025@gmail.com>
2025-08-26 17:37:42 +0900

    chore(user): 회원가입 유효성 검사 변수명 수정

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,13 +1,12 @@
 import { object, create, size, string } from 'superstruct';
 import { CreateUserRequestDTO } from '../types/userType';
 import { utilValidator } from './utilValidator';
-
 /**
  *
  * @param createUserRequestDTO createUserRequestDTO 입력받아 형식을 검증하는 validator 입니다.
  */
 const createUserValidator = (createUserRequestDTO: CreateUserRequestDTO) => {
-  const loginStruct = object({
+  const userStruct = object({
     name: utilValidator.name,
     email: utilValidator.email,
     password: utilValidator.password,
@@ -17,7 +16,7 @@ const createUserValidator = (createUserRequestDTO: CreateUserRequestDTO) => {
     company: utilValidator.companyName,
     companyCode: size(string(), 1, 20), // 별도 제한이 없었던것 같기에 길이만 검사
   });
-  create(createUserRequestDTO, loginStruct);
+  create(createUserRequestDTO, userStruct);
 };
 
 export { createUserValidator };
